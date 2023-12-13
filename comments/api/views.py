b5bc58f75cc1973a8f31d445682a6185d0271695
Andrewzhang217 <zhangguangzesheng@gmail.com>
2023-12-13 22:43:38 +0800

    Inject likes into tweets and comments

--- comments/api/views.py
+++ comments/api/views.py
@@ -36,7 +36,11 @@ class CommentViewSet(viewsets.GenericViewSet):
         comments = self.filter_queryset(queryset) \
             .prefetch_related('user') \
             .order_by('created_at')
-        serializer = CommentSerializer(comments, many=True)
+        serializer = CommentSerializer(
+            comments,
+            context={'request': request},
+            many=True
+        )
         return Response(
             {'comments': serializer.data},
             status=status.HTTP_200_OK,
@@ -60,7 +64,7 @@ class CommentViewSet(viewsets.GenericViewSet):
         # save will call the create() method of serializer to instantiate
         comment = serializer.save()
         return Response(
-            CommentSerializer(comment).data,
+            CommentSerializer(comment, context={'request': request}).data,
             status=status.HTTP_201_CREATED,
         )
 
@@ -77,7 +81,7 @@ class CommentViewSet(viewsets.GenericViewSet):
             }, status=status.HTTP_400_BAD_REQUEST)
         comment = serializer.save()
         return Response(
-            CommentSerializer(comment).data,
+            CommentSerializer(comment, context={'request': request}).data,
             status=status.HTTP_200_OK,
         )
 
