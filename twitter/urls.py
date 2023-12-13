0c50001af45a3bcafcefbb7612c1c5801bdb8cc2
Andrewzhang217 <zhangguangzesheng@gmail.com>
2023-12-13 18:44:14 +0800

    Create api for likes

--- twitter/urls.py
+++ twitter/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from accounts.api.views import UserViewSet, AccountViewSet
-from tweets.api.views import TweetViewSet
-from friendships.api.views import FriendshipViewSet
-from newsfeeds.api.views import NewsFeedViewSet
 from comments.api.views import CommentViewSet
+from friendships.api.views import FriendshipViewSet
+from likes.api.views import LikeViewSet
+from newsfeeds.api.views import NewsFeedViewSet
+from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
@@ -29,6 +30,7 @@ router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
+router.register(r'api/likes', LikeViewSet, basename='likes')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
