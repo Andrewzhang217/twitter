217191d7fd2aa2f53f377b2f119786194f5da2ac
Andrewzhang217 <zhangguangzesheng@gmail.com>
2023-12-16 22:17:36 +0800

    TweetPhoto model and admin

--- tweets/models.py
+++ tweets/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from likes.models import Like
+from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
 from utils import time_helpers
 
 
@@ -34,3 +35,44 @@ class Tweet(models.Model):
     def __str__(self):
         # this would be shown when print the instance
         return f'{self.created_at} {self.user}: {self.content}'
+
+
+class TweetPhoto(models.Model):
+    # photo under which tweet
+    tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null=True)
+
+    # who upload this photo, Although this info can be obtained from tweets, but repeated records in TweetPhoto can be
+    # convenient. For example, if a person often uploads some illegal photos, then the newly uploaded photos by this
+    # user can be marked as the focus of review. Or when we need to ban all photos uploaded by a user,
+    # we can quickly filter through this model.
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    # image file
+    file = models.FileField()
+    order = models.IntegerField(default=0)  # order of photos under a tweet
+
+    # image status, for review
+    status = models.IntegerField(
+        default=TweetPhotoStatus.PENDING,
+        choices=TWEET_PHOTO_STATUS_CHOICES,
+    )
+
+    # soft delete
+    # When a photo is deleted, it will first be marked as deleted and will not be actually deleted immediately.
+    # The purpose of this is that if the true deletion is performed immediately when the tweet is deleted,
+    # it will usually take some time and affect efficiency.
+    # Can use asynchronous tasks to slowly perform deletion in the background.
+    has_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together = (
+            ('tweet', 'order'),
+            ('user', 'created_at'),
+            ('has_deleted', 'created_at'),
+            ('status', 'created_at'),
+        )
+
+    def __str__(self):
+        return f'{self.tweet_id}: {self.file}'
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
-from tweets.models import Tweet
 from datetime import timedelta
-from utils.time_helpers import utc_now
 from testing.testcases import TestCase
+from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
+from tweets.models import Tweet, TweetPhoto
+from utils.time_helpers import utc_now
 
 
 class TweetTests(TestCase):
@@ -25,3 +25,13 @@ class TweetTests(TestCase):
         user2 = self.create_user('user2')
         self.create_like(user2, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
+
+    def test_create_photo(self):
+        # test create tweet photo objects
+        photo = TweetPhoto.objects.create(
+            tweet=self.tweet,
+            user=self.user1,
+        )
+        self.assertEqual(photo.user, self.user1)
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
