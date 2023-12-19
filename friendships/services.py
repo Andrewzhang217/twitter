860b010fd6697e81d44055cfb145dc574439e3ad
Andrewzhang217 <zhangguangzesheng@gmail.com>
2023-12-19 21:26:25 +0800

    Cache followings data

--- friendships/services.py
+++ friendships/services.py
@@ -1,4 +1,9 @@
+from django.core.cache import caches
 from friendships.models import Friendship
+from twitter import settings
+from twitter.cache import FOLLOWINGS_PATTERN
+
+cache = caches['testing'] if settings.TESTING else caches['default']
 
 
 class FriendshipService(object):
@@ -16,4 +21,36 @@ class FriendshipService(object):
             from_user=from_user,
             to_user=to_user,
         ).exists()
+
     # it checks by select count > 0
+
+    @classmethod
+    def get_following_user_id_set(cls, from_user_id):
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        user_id_set = cache.get(key)
+
+        # cache hit
+        if user_id_set is not None:
+            return user_id_set
+
+        # cache miss
+        friendships = Friendship.objects.filter(from_user_id=from_user_id)
+        user_id_set = set([
+            fs.to_user_id
+            for fs in friendships
+        ])
+
+        # load into memcached
+        cache.set(key, user_id_set)
+        return user_id_set
+
+    # remember the cache coherence protocol implemented in CS4223?
+    # MESI for invalidation and Dragon for update
+    # https://github.com/Andrewzhang217/simulator
+    @classmethod
+    def invalidate_following_cache(cls, from_user_id):
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        cache.delete(key)
+
+        # call this method in friendships api would invalidate cache when db is
+        # modified by api call, but admin modification would not trigger this
---
+++ twitter/cache.py
@@ -0,0 +1,4 @@
+# memcached
+FOLLOWINGS_PATTERN = 'followings:{user_id}'
+# FOLLOWERS_PATTERN is not cached because usually followers data are too big, millions
+# also often get stale due to frequent follow/unfollow
